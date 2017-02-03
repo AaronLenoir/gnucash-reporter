c4fd310dbb4c424ac1395d58900cbc4a5074b051
user <user@debian>
2017-02-04 00:42:52 +0100

    Adds mocha test framework.

---
+++ src/test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe("Testing", function () {
+  describe("Test framework", function () {
+    it("Assert of true should always show a passing test.", function () {
+      assert.ok(true);
+    });
+  });
+});
